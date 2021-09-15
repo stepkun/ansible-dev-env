cefb6886296b3e5b0bf603aad28473fe054779c7
stepkun <stephan.kunz@kabelbw.de>
2021-09-15 19:04:37 +0200

    apply conventions

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -3,22 +3,22 @@
 # - check whether already done, if not
 #   - allow password based ssh login by commenting out the appropriate line in /etc/ssh/sshd_config
 #   - create a user 'ansible' with password 'ansible'
-user=ansible
-password=ansible
-if [ -d /home/$user ]
+USER=ansible
+PASSWORD=ansible
+if [ -d /home/$USER ]
 then
-  echo "$user's environment already created"
+  echo "$USER's environment already created"
 else
   sudo sed -i 's/PasswordAuthentication/#PasswordAuthentication/g' /etc/ssh/sshd_config
   sudo service sshd restart
-  useradd -m -s /bin/bash $user
-  adduser $user sudo
-  echo $user:$password | chpasswd
-  mkdir /home/$user/.ssh
-  chown $user.$user /home/$user/.ssh
-  cp /vagrant/ansible/id_ecdsa* /home/$user/.ssh/
+  useradd -m -s /bin/bash $USER
+  adduser $USER sudo
+  echo $USER:$PASSWORD | chpasswd
+  mkdir /home/$USER/.ssh
+  chown $USER.$USER /home/$USER/.ssh
+  cp /vagrant/ansible/id_ecdsa* /home/$USER/.ssh/
   # everything in the .ssh dir should only be accessible by the user
-  chown $user.$user /home/$user/.ssh/*
-  chmod 0600 /home/$user/.ssh/*
-  echo "created $user's environment"
+  chown $USER.$USER /home/$USER/.ssh/*
+  chmod 0600 /home/$USER/.ssh/*
+  echo "created $USER's environment"
 fi
